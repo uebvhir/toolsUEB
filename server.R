7f4946b74b121c49432a7887919f244fb38dccb4
elisabetas87 <elisab.aguayo@gmail.com>
2019-03-08 13:00:08 +0100

    first steps with the view

--- server.R
+++ server.R
@@ -2,6 +2,13 @@ library(shiny)
 
 shinyServer(function(input,output){
   
- 
+  rawData <- reactive({
+    validate(
+      need(input$file != "", "Please Load TopTable files")
+    )
+    
+    read.csv(input$file$datapath)
+    
+  })
   
 })
--- ui.R
+++ ui.R
@@ -1,3 +1,10 @@
+options(spinner.color="#00ff83")
+options(shiny.maxRequestSize = 70*1024^2)
+options(expressions = 5000)
+options(repos = BiocInstaller::biocinstallRepos())
+getOption("repos")
+
+
 library(shiny)
 library(shinydashboard)
 
@@ -15,4 +22,55 @@ sidebar <- dashboardSidebar(
 )
 )
 
-body <- dashboardBody()
+body <- dashboardBody(
+  tabItems(
+   
+     ##introduction
+    tabItem(tabName = "intro",
+            tags$img("Desktop/projecte/index.png"),
+            br(),
+            h1("UEB Tools App"),
+            br(),
+            p("This app allows you to upload a file and doing modifications...")
+          ),
+   
+     ##upload files
+    tabItem(tabName = "upfiles", fluidRow(
+      box(title = "Input Data",
+          solidHeader = T, status = "warning", width = 12,
+          
+          fluidRow(
+            ###Input TopTable files###
+            box(title ="TopTable FILES",
+                solidHeader = T, status = "info", width = 6,
+                
+                fileInput("file", "Load TopTable files(.csv)",
+                          multiple = TRUE,
+                          accept = ".csv",
+                          placeholder = "Please, insert the datapath of Top Table files"),
+                submitButton("Submit")
+            )
+            
+          )
+        )
+  
+     )
+)
+  ##filters
+   #tabItem(tabName = "filters", fluidRow(
+    # box(title = "filtering",
+     #    solidHeader = T, status = warning, width = 12,
+         
+      #   fluidRow(
+       #    box(title = "filter contitions",
+        #       solidHeader = T, status = "info", width = 6
+               
+         #      )
+        
+         #)
+         #)
+  # ))
+
+)
+)
+ui <- dashboardPage(header, sidebar, body, skin = "green")

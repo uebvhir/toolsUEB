9b641d9c76282d3dcd1f41b7707d3a5ca82de3c0
elisabetas87 <elisab.aguayo@gmail.com>
2019-05-06 12:48:12 +0200

    filtres funcionant

--- server.R
+++ server.R
@@ -1,35 +1,88 @@
 library(shiny)
 
 shinyServer(function(input,output){
-##load FILES
+######load FILES
   #load TopTable Files
-  dataT <- reactive({
+  topTab <- reactive({
     validate(
-      need(input$file != "", "Please Load TopTable files")
+      need(input$file1 != "", "Please Load TopTable files")
     )
-    
-    read.csv(input$file$datapath,sep = ";")
+    inFile1 <- input$file1
+    if (is.null(inFile1)) return(invisible(NULL))
+    else {read.csv2(inFile1$datapath, sep=input$sep1, dec=input$dec1, header=TRUE)}
     })
   
   #Load Expression Matrix Files
-  genes <- reactive({
+  expMat <- reactive({
     validate(
       need(input$file2 !="", "Please load the Expression Matrix file")
     )
-    
-    genes1 = input$file2
-    data1 = read.csv(genes1$datapath,sep = ";")
-    return(data1)
+    inFile2 <- input$file2
+    if (is.null(inFile2)) return(invisible(NULL))
+    else {read.csv2(inFile2$datapath, sep=input$sep2, dec=input$dec2, header=TRUE)}
   })
   
-  #####Custom targets information#####
+  #####Custom topTable information#####
   
-  output$dataT <- renderDataTable({
-    dataT()
+  output$topTab <- renderDataTable({
+    topTab()
   })
   
 ##Analysis
   ##Graphical analysis
   
+  #####Selected genes table######
   
+  selected <- reactive({
+    
+    if(input$pvalue == "0.05"){
+      if(input$lfc == "1"){
+        sel1 <- topTab()[topTab()[, "adj.P.Val"] < 0.05, ]
+        selected <- sel1[sel1[, "logFC"] > 1, ]
+      }else if(input$lfc == "-1"){
+        sel1 <- topTab()[topTab()[, "adj.P.Val"] < 0.05, ]
+        selected <- sel1[sel1[, "logFC"] < -1, ]
+      }else if(input$lfc == "no filter"){
+        selected <- topTab()[topTab()[, "adj.P.Val"] < 0.05, ]
+      }
+   }else if(input$pvalue == "0.001"){
+      if(input$lfc == "1"){
+        sel1 <- topTab()[topTab()[, "adj.P.Val"] < 0.001, ]
+        selected <- sel1[sel1[, "logFC"] > 1, ]
+      }else if(input$lfc == "-1"){
+        sel1 <- topTab()[topTab()[, "adj.P.Val"] < 0.001, ]
+        selected <- sel1[sel1[, "logFC"] < -1, ]
+      }else if(input$lfc == "no filter"){
+        selected <- topTab()[topTab()[, "adj.P.Val"] < 0.001, ]
+      }
+   }else if(input$pvalue == "no filter"){
+     if(input$lfc == "1"){
+       selected <- topTab()[topTab()[, "logFC"] > 1, ]
+     }else if(input$lfc == "-1"){
+       selected <- topTab()[topTab()[, "logFC"] < -1, ]
+     }else if(input$lfc == "no filter"){
+       selected <- topTab()
+     }
+    }
+    
+  })
+  
+   #####Output selected genes table#######
+  
+  output$selectedtable <- renderDataTable({
+    data <- selected()[,c(1:6)]
+    
+    datatable(data, rownames = FALSE)
+  })
+ 
+  #####Volcano Plot#######
+
+  output$volcano <- renderPlot({
+    y <- as.numeric(data()$P.Value)
+    x <- data()$logFC
+    with(data(), plot(x, -log10(y), pch=20, main="Differentially expressed genes", xlim=c(-2.5,2),ylim=c(-2.5,2))) 
+               
+    #
+    abline(v=c(-1,1))
+  })
 })
--- ui.R
+++ ui.R
@@ -7,14 +7,15 @@ getOption("repos")
 #libraries
 library(shiny)
 library(shinydashboard)
-#source("http://bioconductor.org/biocLite.R")
-#biocLite("oligo")
-#biocLite("pd.mogene.1.0.st.v1")
-#biocLite("arrayQualityMetrics")
-#biocLite("genefilter")
-#biocLite("limma")
-#biocLite("xtable")
-#biocLite("gplots")
+library(DT)
+source("http://bioconductor.org/biocLite.R")
+biocLite("oligo")
+biocLite("pd.mogene.1.0.st.v1")
+biocLite("arrayQualityMetrics")
+biocLite("genefilter")
+biocLite("limma")
+biocLite("xtable")
+biocLite("gplots")
 
 
 header <- dashboardHeader(
@@ -23,10 +24,10 @@ header <- dashboardHeader(
 
 sidebar <- dashboardSidebar(
   sidebarMenu(id="tabs", menuItem("Introduction", tabName = "intro", icon = icon("edit")),
-                          menuItem("Upload Files", tabName = "upfiles", icon = icon("server")),
-                          menuItem("Analysis", tabName = "analysis", icon = icon("braille"),
+                         menuItem("Upload Files", tabName = "upfiles", icon = icon("server")),
+                         menuItem("Analysis", tabName = "analysis", icon = icon("braille"),
                                               menuSubItem("Graphics", tabName = "graphics"),
-                                               menuSubItem("Functional", tabName = "graphics2")
+                                              menuSubItem("Functional", tabName = "graphics2")
                                     ))
 )
 
@@ -48,17 +49,19 @@ body <- dashboardBody(
           solidHeader = T, status = "success", width = 12,
           
           fluidRow(
-            ###Input TopTable files###
+            ####Input TopTable files####
             box(title ="TopTable FILES",
                 solidHeader = T, status = "info", width = 6,
                 
-                fileInput("file", "Load TopTable files(.csv)",
+                fileInput("file1", "Load TopTable files(.csv)",
                           multiple = TRUE,
                           accept = ".csv",
                           placeholder = "Please, insert the datapath of Top Table files"),
+                radioButtons('sep1', 'Separator', c('Tab' ='\t','Comma (,)' =',', 'Semicolon (;)' =';'),"\t"),
+                radioButtons('dec1', 'Decimal',   c('Point (.)'='.', 'Comma (,)'=','), '.'),
                 submitButton("Submit")
             ),
-            ###Input Expression Matrix files###
+            ####Input Expression Matrix files####
             box(title ="Expression Matrix FILES",
                 solidHeader = T, status = "info", width = 6,
                 
@@ -66,6 +69,8 @@ body <- dashboardBody(
                           multiple = TRUE,
                           accept = ".csv",
                           placeholder = "Please, insert the datapath of Matrix Expression files"),
+                radioButtons('sep2', 'Separator', c('Tab' ='\t','Comma (,)' =',', 'Semicolon (;)' =';'),"\t"),
+                radioButtons('dec2', 'Decimal',   c('Point (.)'='.', 'Comma (,)'=','), '.'),
                 submitButton("Submit")
           )
         )
@@ -79,26 +84,41 @@ body <- dashboardBody(
            h2("Results"),
             fluidRow(
               box(title = "Toptable",
-               solidHeader = TRUE, status = "success", width = 12,
+               solidHeader = T, status = "success", width = 12,
                 fluidRow(
                   box(title = "Top Table",
                      solidHeader = T, status = "info", width = 12,
-                      dataTableOutput("dataT")),
+                      dataTableOutput("topTab")),
+                  
                   box(title = "Selected Genes",
                       solidHeader = T, status = "info", width = 3,
-                      numericInput("pvalue", "Select Adjust P Value:",value = 0.05, min = 0.001, max= 0.7),
-                      radioButtons("logfc", "Select log FC:", c("> 1" = "greater1",
-                                                              "< -1" = "infm1")),
-                      numericInput("ngenes","Select Number of Genes", value =10,min = 1, max = 20),
+                      selectInput("pvalue","Adj.p-val",choices=list("no filter","0.001","0.05"), 
+                                  selected = "no filter", multiple = FALSE,
+                                  selectize = TRUE, width = NULL, size = NULL),
+                      selectInput("lfc", "Select LFC: ", choices=list("no filter","1","-1"), 
+                                  selected = "no filter", multiple = FALSE,
+                                  selectize = TRUE, width = NULL, size = NULL),
                       submitButton("Submit")),
                   box(title = "Selected Genes from Top Table",
                      solidHeader = T, status = "info", width = 9,
-                     dataTableOutput("selectedtable"),
-                     downloadButton("table2", label = "Download"))
-                 ))
-               ))
+                     dataTableOutput("selectedtable")
+                     )
+                 )
+              ),
+              box(title = "Volcano plot", 
+                  solidHeader = T, status="success",width = 12,
+                  (plotOutput("volcano")),
+                  sliderInput("volcano", "number of genes:",
+                              min = 1, max = 10,
+                              value = 2,step = 1),
+                  submitButton("Submit")
+                  
+                  )
+            )
+        )
+)
+)
+               
 
-)
-)
 
 ui <- dashboardPage(header, sidebar, body, skin = "green")
